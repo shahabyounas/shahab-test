91fceff6840f96b7b84e6972d04f9189f17168f5
shahabyounas <shahabyounas@users.noreply.github.com>
2023-03-16 16:06:46 +0000

    Import dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+/**
+ * Import Font Awsome
+ */
+
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+
+/**
+ * Import Bootstrap CSS and bundle
+ */
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
